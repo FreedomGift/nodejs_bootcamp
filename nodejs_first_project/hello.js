f29aea3f532af8958d29be63cd304adc13967ddd
Andreas Salim <andreas.wendysastra.salim@gmail.com>
2025-10-17 07:25:01 +0700

    feat: add greeting logic based on time of day

--- nodejs_first_project/hello.js
+++ nodejs_first_project/hello.js
@@ -1,3 +1,19 @@
 //My first node.js code
 console.log("Hello from Node.js!!");
 
+const userName = "Andreas";
+const currentTime = new Date();
+const hours = currentTime.getHours();
+console.log(hours);
+let greeting;
+if (hours >= 5 && hours < 12) {
+    greeting = "Good morning";
+} else if (hours >= 12 && hours < 18) {
+    greeting = "Good afternoon";
+} else if (hours >= 18 && hours < 22) {
+    greeting = "Good evening";
+} else {
+    greeting = "Good night";
+}
+
+console.log(`${greeting}, ${userName}! Welcome to NodeJS.`);
