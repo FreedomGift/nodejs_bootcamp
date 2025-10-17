68133bf229fec17cfaf51535cde7e83d3c3e0989
Andreas Salim <andreas.wendysastra.salim@gmail.com>
2025-10-17 09:22:07 +0700

    feat: Add es6 module example with math functions

---
+++ modules/es6_module/app.mjs
@@ -0,0 +1,9 @@
+import * as math from './math.mjs';
+
+console.log(math.add(1, 2));
+console.log(math.subtract(1, 2));
+console.log(math.multiply(1, 2));
+console.log(math.divide(1, 2));
+console.log(math.version);
+console.log(math.name);
+
