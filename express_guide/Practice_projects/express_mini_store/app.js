021345fb11bd99cb7356a21528d98a1853633db5
Andreas Salim <andreas.wendysastra.salim@gmail.com>
2025-10-20 12:35:46 +0700

    fix: Memperbaiki sintaks rute dan menambahkan rute produk

--- express_guide/Practice_projects/express_mini_store/app.js
+++ express_guide/Practice_projects/express_mini_store/app.js
@@ -10,18 +10,24 @@ app.get("/", (_req, res)=>{
 app.get("/products", (_req, res)=>{
   res.send("Here are our amazing products");
 });
+
 //! About Route
-app.get("/about", (_req, res)=>{
+app.get("/about", (_req, res) => {
   res.send("This our story company: We sell the quality products");
-}
-);
-app.get("/contact",(_eq, res) =>{
-  res.send("Please always be in touch with our products, don't hasitate to contact us at masyntech@pxdmail.com");
-}
-);
+});
 
+app.get("/contact", (_req, res) => {
+  res.send("Please always be in touch with our products, don't hesitate to contact us at masyntech@pxdmail.com");
+});
 
+// Route parameters
+app.get("/product/:productId", (_req, res) => {
+  console.log(_req.params.productId);
+  res.send(`This is product with id: ${_req.params.productId}`);
+});
 app.listen(3000, ()=>{
   console.log("Server is up and running at http://localhost:3000");
-
 })
+
+//products/10
+
