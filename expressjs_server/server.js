aea543457edabc66dc20c59fcfebc73ad1247ea4
Andreas Salim <andreas.wendysastra.salim@gmail.com>
2025-10-21 18:24:09 +0700

    Terapkan perubahan kode untuk meningkatkan fungsionalitas dan meningkatkan kinerja

--- expressjs_server/server.js
+++ expressjs_server/server.js
@@ -3,9 +3,29 @@ import express from 'express';
 const app = express();
 
 //Basic route
-app.get('/', (req, res) => {
+//GET - Get data
+//POST - Send data
+//PUT/PATCH - Update data
+//DELETE - Delete data
+app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
+
+//About route
+app.get('/about', (_req, res) => {
+  res.send('About Us page.');
+});
+
+//Contact route
+app.get('/contact', (_req, res) => {
+  res.send('This is the contact page.');
+});
+
+//Register route
+app.post('/register', (_req, res) => {
+  res.send('User registration successful.');
+});
+
 
 //Start the server
 app.listen(3000, () => console.log('Server is running on port http://localhost:3000'));
