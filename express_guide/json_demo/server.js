e78ebdfc697824859156b9342744458696c294e6
Andreas Salim <andreas.wendysastra.salim@gmail.com>
2025-10-21 19:27:29 +0700

    Tambahkan pengaturan awal Express.js dengan package.json dan server.js
    - Membuat package.json untuk dependensi dan metadata proyek
    - Menerapkan server.js untuk menginisialisasi aplikasi Express dan menentukan rute dasar

---
+++ express_guide/json_demo/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+//!Initialize express app
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello, World!', author: "Andreas" }, 200);
+});
+//!Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
