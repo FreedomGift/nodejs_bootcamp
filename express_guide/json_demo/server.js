db9202ad3d0f7faf54b9a754c0e1ac4aad85e8e2
Andreas Salim <andreas.wendysastra.salim@gmail.com>
2025-10-22 13:10:50 +0700

    feat: Menambahkan rute POST untuk buku dan rute GET dasar

--- express_guide/json_demo/server.js
+++ express_guide/json_demo/server.js
@@ -2,10 +2,23 @@ const express = require('express');
 //!Initialize express app
 const app = express();
 const PORT = 3000;
-
+//Sending JSON data as response
 app.get('/', (_req, res) => {
   res.json({ message: 'Hello, World!', author: "Andreas" }, 200);
 });
+//Making POST request
+//req.body contain the data sent by client is sending to server for processing
+ //pass inconming request body to book variable
+ app.use(express.json());
+app.post("/books", (req, res) => {
+
+  res.json(
+    {
+      message: "Book added succesfully ", data: req.body
+    }
+  );
+} );
+
 //!Start server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
